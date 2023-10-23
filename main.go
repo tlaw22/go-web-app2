80d4a082d92932a772d2de17fc6fb2324c83030b
netadmin <timothy.j.lawless@outlook.com>
2023-10-23 14:32:08 -0400

    Added error checking for dividing by zero. Lesson complete.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -15,6 +16,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
 	sum := addValues(2, 2)
+
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 = %d", sum))
 }
 
@@ -22,10 +24,28 @@ func About(w http.ResponseWriter, r *http.Request) {
 func addValues(x, y int) int {
 	return x + y
 }
+func Divvide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by zero")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
+}
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by Zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/About", About)
+	http.HandleFunc("/divide", Divvide)
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	fmt.Println()
 	_ = http.ListenAndServe(portNumber, nil)
 }
 
