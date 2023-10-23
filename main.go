01534efc8161fa9a432292b256531cc57715733f
Tim <timothy.j.lawless@outlook.com>
2023-10-23 08:51:09 -0400

    Lesson complete

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
 // Home is the homepage handler
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is a home page")
@@ -13,7 +15,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
 	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 - %d", sum))
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 = %d", sum))
 }
 
 // AddValues adds 2 values togeter
@@ -23,8 +25,8 @@ func addValues(x, y int) int {
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/About", About)
-
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
 
 // Naming a function with a lowercase start letter makes the function private
